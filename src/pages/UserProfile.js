eb57c24c0ec3efa987e193c23dc8ef8bd1fc7144
Xennia Contreras <Xenbydesign@gmail.com>
2024-03-02 11:31:19 -0800

    done

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,20 +1,13 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useOutletContext, useParams } from "react-router-dom";
 
 function UserProfile() {
-  const [user, setUser] = useState({});
   const params = useParams();
-  const userId = params.id;
+  const users = useOutletContext();
 
-  useEffect(() =>{
-    fetch(`http://localhost:4000/users/${userId}`)
-    .then(r => r.json())
-    .then(data => setUser(data))
-    .catch(error => console.error(error));
-  }, [userId]);
+  const user = users.find(user => user.id === parseInt(params.id)); 
 
-  if(!user.name){
-    return <h1>Loading...</h1>;
+  if(!user){
+    return <h1>Loading...</h1>
   };
 
   return(  
@@ -25,3 +18,4 @@ function UserProfile() {
 };
 
 export default UserProfile;
+
